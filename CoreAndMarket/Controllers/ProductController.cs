6baf8145418087bc0858520de8789cb1f4f2bc07
BKarayilmaz <63595909+BKarayilmaz@users.noreply.github.com>
2022-01-15 15:34:58 +0300

    Add Product Image File and Panel Login UI

--- CoreAndMarket/Controllers/ProductController.cs
+++ CoreAndMarket/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using X.PagedList;
@@ -32,9 +33,25 @@ namespace CoreAndMarket.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult AddProduct(Product pr)
+        public IActionResult AddProduct(AddProduct ap)
         {
-            productRepository.AddT(pr);
+            Product p = new Product();
+            if (ap.ImageUrl!=null)
+            {
+                var extension = Path.GetExtension(ap.ImageUrl.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/", newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+                ap.ImageUrl.CopyTo(stream);
+                p.ImageUrl = newImageName;
+            }
+            p.ProductName = ap.ProductName;
+            p.Price = ap.Price;
+            p.Stock = ap.Stock;
+            p.CategoryID = ap.CategoryID;
+            p.Description = ap.Description;
+
+            productRepository.AddT(p);
             return RedirectToAction("Index");
         }
 
@@ -48,16 +65,27 @@ namespace CoreAndMarket.Controllers
         public IActionResult GetProduct(int id)
         {
             var x = productRepository.GetT(id);
+
             Product p = new Product()
             {
                 CategoryID = x.CategoryID,
                 Stock = x.Stock,
                 Price = x.Price,
-                Description=x.Description,
+                Description = x.Description,
                 ProductID = x.ProductID,
                 ProductName = x.ProductName,
-                ImageUrl=x.ImageUrl
+                ImageUrl = x.ImageUrl
             };
+
+            //AddProduct addProduct = new AddProduct()
+            //{
+            //    Stock = x.Stock,
+            //    Price = x.Price,
+            //    Description = x.Description,
+            //    ProductID = x.ProductID,
+            //    ProductName = x.ProductName,
+            //    CategoryID = x.CategoryID,
+            //};
             List<SelectListItem> values = (from y in c.Categories.ToList()
                                            select new SelectListItem
                                            {
@@ -68,6 +96,19 @@ namespace CoreAndMarket.Controllers
             return View(p);
         }
 
+        //public IActionResult UpdateProduct(Product p)
+        //{
+        //    var x = productRepository.GetT(p.ProductID);
+        //    x.ProductName = p.ProductName;
+        //    x.Stock = p.Stock;
+        //    x.Price = p.Price;
+        //    x.ImageUrl = p.ImageUrl;
+        //    x.Description = p.Description;
+        //    x.CategoryID = p.CategoryID;
+
+        //    productRepository.UpdateT(x);
+        //    return RedirectToAction("Index");
+        //}
         public IActionResult UpdateProduct(Product p)
         {
             var x = productRepository.GetT(p.ProductID);
@@ -79,6 +120,25 @@ namespace CoreAndMarket.Controllers
             x.CategoryID = p.CategoryID;
 
             productRepository.UpdateT(x);
+
+            ////Product p = new Product();
+            //var p = productRepository.GetT(ap.ProductID);
+            //if (ap.ImageUrl != null)
+            //{
+            //    var extension = Path.GetExtension(ap.ImageUrl.FileName);
+            //    var newImageName = Guid.NewGuid() + extension;
+            //    var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images/", newImageName);
+            //    var stream = new FileStream(location, FileMode.Create);
+            //    ap.ImageUrl.CopyTo(stream);
+            //    p.ImageUrl = newImageName;
+            //}
+            //p.ProductName = ap.ProductName;
+            //p.Price = ap.Price;
+            //p.Stock = ap.Stock;
+            //p.CategoryID = ap.CategoryID;
+            //p.Description = ap.Description;
+
+            //productRepository.UpdateT(p);
             return RedirectToAction("Index");
         }
     }
